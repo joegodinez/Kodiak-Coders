b0a31935880524e38b02c3d28b3ac7c4447a629a
CasualGamer1221 <parentjeffery@gmail.com>
2023-04-09 23:00:46 -0400

    Location and places implementation started

---
+++ TravelGavel/app/src/main/java/com/example/travelgavel/MapsActivity.java
@@ -0,0 +1,135 @@
+package com.example.travelgavel;
+
+
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationRequest;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.travelgavel.databinding.ActivityMapsBinding;
+import com.google.android.gms.tasks.Task;
+
+import java.security.Permission;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private static final int Request_code = 101;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this.getApplicationContext());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    private void getCurrentLocation(){
+
+        if(ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+        ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Request_code});
+            return;
+
+        }
+
+        LocationRequest locationRequest= new LocationRequest.create();
+        locationRequest.setInterval(60000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setFast;
+        LocationCallback locationCallback= new LocationCallback() {
+
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                Toast.makeText(getApplicationContext(),"location result is=" + locationResult
+                ,Toast.LENGTH_LONG).show();
+
+                if(locationResult== null){
+
+                    Toast.makeText(getApplicationContext(), "Current location is null"
+                            ,Toast.LENGTH_LONG).show();
+
+                    return;
+                }
+
+                for(Location location:locationResult.getLocations()){
+
+                    if(location!=null){
+                        Toast.makeText(getApplicationContext(), "Current location is" + location.getLongitude()
+                                ,Toast.LENGTH_LONG).show();
+
+                    }
+                }
+            }
+        };
+
+        fusedLocationProviderClient.requestLocationUpdates
+                (locationRequest, locationCallback, null);
+        Task<Location> task=fusedLocationProviderClient.getLastLocation();
+
+
+
+    }
+
+    @SuppressLint("MissingSuperCall")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (Request_code){
+
+            case Request_code:
+                if(grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+
+                    getCurrentLocation();
+                }
+        }
+    }
+}
